713317cf9008a62a188ba51d3c8ed74b12b7e66a
Luca Georges Francois <luca@quartz.technology>
2024-01-12 19:03:01 +0100

    feat(indexer): add beacon api client errors
    Signed-off-by: Luca Georges Francois <luca@quartz.technology>

---
+++ indexer/beacon_api_client_errors.go
@@ -0,0 +1,15 @@
+package indexer
+
+import "fmt"
+
+// NewBeaconAPIClientInitializationError is raised if the client used to connect to the beacon
+// node fails to initialize.
+func NewBeaconAPIClientInitializationError(err error) error {
+	return fmt.Errorf("failed to initialize beacon API client service: %w", err)
+}
+
+// NewEventSubscriptionError is raised if the client connected to the node fails to create an event
+// subscription.
+func NewEventSubscriptionError(err error) error {
+	return fmt.Errorf("failed to create event subscription: %w", err)
+}
