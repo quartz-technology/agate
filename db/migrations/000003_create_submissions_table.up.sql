4744de64a12bb8670fde27d933675e54b4a02772
Luca Georges Francois <luca@quartz.technology>
2024-01-20 14:58:47 +0100

    fix(database): simplify migrations
    Signed-off-by: Luca Georges Francois <luca@quartz.technology>

--- db/migrations/000003_create_submissions_table.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS submissions(
-    id BIGSERIAL PRIMARY KEY,
-    relay_id INTEGER NOT NULL,
-    bid_block_hash BYTEA NOT NULL,
-    is_delivered BOOLEAN NOT NULL,
-    is_optimistic BOOLEAN NOT NULL,
-    submitted_at TIMESTAMP NOT NULL
-);
